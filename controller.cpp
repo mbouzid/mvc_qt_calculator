27d9d5e687c66276c4183a59b7e0e57240b929c9
Mariam Bouzid <mariam.bouzid@icloud.com>
2016-03-01 22:02:17 +0100

    Create controller.cpp

---
+++ controller.cpp
@@ -0,0 +1,13 @@
+#include "controller.hpp"
+const char * controller::slots_name[9] =
+{
+	"1add_one()",
+	"1add_two()",
+	"1add_three()",
+	"1add_four()",
+	"1add_five()",
+	"1add_six()",
+	"1add_seven()",
+	"1add_eight()",
+	"1add_nine()"
+};
