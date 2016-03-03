31bd9e4b789397a9629f3fece5ab439b0a94476b
Mariam Bouzid <mariam.bouzid@icloud.com>
2016-03-03 20:30:48 +0100

    Update model.hpp

--- model.hpp
+++ model.hpp
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <sstream>
 #include <assert.h>
+#include <model_exception.hpp>
 
 enum typeOp {
 	PLUS,
@@ -26,22 +27,29 @@ class Calc
 
 	public:
 		Calc();
-		void addChar(const char);
+		void addChar(const char) throw (model_exception&);
 		void pushValue();
 		
 		int opPriority(const int op);
-		int opArite(const int op);
-		void doCompute(const int nextOpe);
+		int opArite(const int op) throw (model_exception&);
+		void doCompute(const int nextOpe) throw (model_exception&) ;
 		
 		void plus();
 		void minus();
 		void times();
-		void divide();
+		void divide() throw (model_exception&);
 		void square();
-		double equal();
+		double equal() throw (model_exception&) ;
 
 		void bracketL();
 		void bracketR();
-
+		
+		
+		bool hasResult() const
+		{
+			return (_stackValues.size() == 1 );
+		}
+		
+		~Calc()
+		{}
 };
-
