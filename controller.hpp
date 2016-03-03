1465ef60d33e77a12263def36cefd0ba9702e64d
Mariam Bouzid <mariam.bouzid@icloud.com>
2016-03-03 20:30:23 +0100

    Update controller.hpp

--- controller.hpp
+++ controller.hpp
@@ -2,168 +2,113 @@
 
 #include <QtCore/QObject>
 #include <QtCore/QString>
+#include <QtGui/QPushButton>
 
 #include "model.hpp"
-
 
 class controller : public QObject
 {
 	Q_OBJECT
-	
-	public :
-	
-		controller():m_model()
-		{
 			
-		}
-		
-		static const char * slots_name[9];
 	private :
 	
 		Calc m_model ;
+		
+	public :
+	
+		controller():m_model()
+		{}
+		
+		~controller()
+		{}
+
 	
 	public slots :
 	
-		
-	
-	
-		void add_comma()
+		void add() 
 		{
-			m_model.addChar('.');
-			emit change_text(",");
-		}
-		
-		void add_zero( )
-		{	
-			m_model.addChar('0');
-			emit change_text(QString::number(0));
+			QPushButton * button = qobject_cast<QPushButton*>(sender());
 			
-		}
-		
-		void add_one( )
-		{	
-			m_model.addChar('1');
-			emit change_text(QString::number(1));
+			if ( button )
+			{
+				
+				emit change_text(button->text());
+				char label = button->text().at(0).toAscii() ;
+				if ( (label >= '0') && ( label <='9' ) )
+				{
+					m_model.addChar(button->text().at(0).toAscii());
+				}
+				else
+				{
+					
+					if ( m_model.hasResult() )
+					{
+						emit result(QString::number(m_model.equal()));
+					}
+					
+					switch ( label )
+					{
+						case '+' :
+						{
+							m_model.plus();
+							break ;
+						}
+						case '-' :
+						{
+							m_model.minus();
+							break;
+						}
+						case '*' :
+						{
+							m_model.times();
+							break;
+						}
+						case '/' :
+						{
+							m_model.divide();
+							break;
+						}
+						case '^' :
+						{	
+							m_model.square();
+							break;
+						
+						}
+						case '=' :
+						{
+							emit result(QString::number(m_model.equal()));
+							
+							break;
+						
+						}
+						
+						case '(' :
+						{
+							m_model.bracketL();
+							break;
+						}
+						
+						case ')' :
+						{
+							m_model.bracketR();
+							break;
+						}
+						
+						
+						
+					}	
+				
+					
+				}
 			
+			}
 		}
-		
-		void add_two( )
-		{	
-			m_model.addChar('2');
-			emit change_text(QString::number(2));
-			
-		}
-		
-		void add_three( )
-		{	
-			m_model.addChar('3');
-			emit change_text(QString::number(3));
-			
-		}
-		
-		void add_four( )
-		{	
-			m_model.addChar('4');
-			emit change_text(QString::number(4));
-			
-		}
-		
-		void add_five( )
-		{	
-			m_model.addChar('5');
-			emit change_text(QString::number(5));
-			
-		}
-		
-		void add_six( )
-		{	
-			m_model.addChar('6');
-			emit change_text(QString::number(6));
-			
-		}
-		
-		void add_seven( )
-		{	
-			m_model.addChar('7');
-			m_model.pushValue();
-			emit change_text(QString::number(7));
-			
-		}
-		
-		void add_eight( )
-		{	
-			m_model.addChar('8');
-			emit change_text(QString::number(8));
-			
-		}
-		
-		void add_nine( )
-		{	
-			m_model.addChar('9');
-			emit change_text(QString::number(9));
-			
-		}
-		
-		
-		void add_plus()
-		{
-				m_model.plus()  ;
-				emit change_text("+");
-		}
-		
-		void add_minus(  )
-		{
-				m_model.minus() ;
-				emit change_text("-");
-		}
-		
-		void add_times( )
-		{
-				m_model.times()  ;
-				emit change_text("*");
-		}
-		
-		void add_divide( )
-		{
-				m_model.divide() ;
-				emit change_text("/");
-		}
-		
-		void equal( )
-		{
-				emit change_text("=");
-				emit change_text( QString::number(m_model.equal()) ) ;
-		}
-		
-		void add_bracket_l( )
-		{
-				m_model.bracketL() ;
-				emit change_text(")");
-		}
-		
-		void add_bracket_r(  )
-		{
-				m_model.bracketR() ;
-				emit change_text("(");
-		}
-		
-		void add_square( )
-		{
-				emit change_text("^2");
-				m_model.square() ;
-		}
-		
-		
 		
 		
          
 	signals :
 		void result( const QString & res ) ;
 		void change_text( const QString & expr);
-	
 
-		
-		
-	
 	
 };
